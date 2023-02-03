2ef936dfb47553aab353c045ec20bf0ac9efc236
antoine <antoine.frisco@gmail.com>
2023-02-03 12:08:12 +0100

    added fetch json

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,35 +1,24 @@
 import { useState, useEffect } from "react";
 import "../styles/gallery.css";
-import testImg from "../assets/test_img.jpg";
+
 
 function Gallery() {
-  useEffect(() => {}, []);
+  const [Data, setData] = useState([]);
+  useEffect(() => {
+    fetch(`/logements.json`)
+      .then((response) => response.json())
+      .then((Data) => setData(Data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="gallery_wrapper">
-      <div className="gallery_card">
-        <h1>nom</h1>
-        <img src={testImg} className="gallery_img"></img>
-      </div>
-      <div className="gallery_card">
-        <h1>nom</h1>
-        <img src={testImg} className="gallery_img"></img>
-      </div>
-      <div className="gallery_card">
-        <h1>nom</h1>
-        <img src={testImg} className="gallery_img"></img>
-      </div>
-      <div className="gallery_card">
-        <h1>nom</h1>
-        <img src={testImg} className="gallery_img"></img>
-      </div>
-      <div className="gallery_card">
-        <h1>nom</h1>
-        <img src={testImg} className="gallery_img"></img>
-      </div>
-      <div className="gallery_card">
-        <h1>nom</h1>
-        <img src={testImg} className="gallery_img"></img>
-      </div>
+      {Data.length > 0 && Data.map(logement => 
+        <div className="gallery_card">
+          <h1>{logement.title}</h1>
+          <img src={logement.cover} className="gallery_img"></img>
+        </div>
+      )}
     </div>
   );
 }
