7c59a8ce2035892c66098fe63349e1dae925327f
antoine <antoine.frisco@gmail.com>
2023-03-03 11:41:16 +0100

    fixed stars

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -1,42 +1,11 @@
 import { useEffect, useState } from "react";
 
-function Ratings(props) {
-
-let [number, setNumber ] = useState([0,0,0,0,0].fill(1,0,props.starNumber).fill(0,props.starNumber,5))
-
-/* useEffect(()=> {
-  let result = []
-  for (let i = 0; i < 5; i += 1) {
-    if (i < props.starNumber) {
-      result.push(1)
-    } 
-    else {
-      result.push(0)
-      
-    }
-  }
-  setNumber(result)
-}, [setNumber]) */
-
+function Ratings({starNumber}) {
 return (
   <div>
-  {Object.entries(number).map(([k,i]) => <i key={"star-" + k} className={`fa-solid fa-star ${i === 1? "red" : "grey"}`}></i>)}
+  {Object.entries([0,0,0,0,0].fill(1,0,starNumber).fill(0,starNumber,5)).map(([k,i]) => <i key={"star-" + k} className={`fa-solid fa-star ${i === 1? "red" : "grey"}`}></i>)}
   </div>
 )
-
-  /* let stars = "";
-
-
-  for (let i = 0; i < 5; i += 1) {
-    if (i < starNumber) {
-      stars += ('<i class="fa-solid fa-star red"></i>')
-    } 
-    else {
-      stars += ('<i class="fa-solid fa-star grey"></i>')
-    }
-  }
-return (stars) */
-  
 }
 
 export default Ratings;
